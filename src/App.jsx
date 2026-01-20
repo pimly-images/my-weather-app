5f5b120f855658f81b132cc07bda9a8b2df83842
pimly <adg4618@gmail.com>
2026-01-20 15:08:01 +0900

    add tomorrow forecast

--- src/App.jsx
+++ src/App.jsx
@@ -95,11 +95,12 @@ const getBackgroundColor = (weather) => {
     <div style={{
   minHeight: '100vh',
   padding: '20px',
-  // 一旦、関数を通さずに直接色を指定して、色が変わるかチェック！
-  backgroundColor: 'grey', 
+  // weatherDataの中から現在の天気を取得して関数に渡すお
+background: getBackgroundColor(weatherData?.weather?.[0]?.main || 'default'),
+  transition: 'background 0.5s ease',
   color: 'white',
   textAlign: 'center'
-    }}>
+}}>
 
 <div style={{ display: 'flex', 
   flexDirection: 'column', 
