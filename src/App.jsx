49c48181c15a84cd942ca542b6d8e2176703c25a
pimly <pimly@pimlynoMacBook-Air.local>
2026-01-20 14:28:52 +0900

    add tomorrow forecast

--- src/App.jsx
+++ src/App.jsx
@@ -184,7 +184,7 @@ function App() {
         </div>
       </div>
 
-<div style={{ width:'60%',
+<div style={{ width:'100%',
   backgroundColor: 'rgba(255, 255, 255, 0.1)', 
   padding: '15px', 
   borderRadius: '10px', 
